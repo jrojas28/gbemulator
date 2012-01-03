3198089a8aab7e0b38e19892ab3493537dcdc0a5
je.suis.bon <je.suis.bon@80cfc581-bd49-91d5-5bb9-f6542bbd8e64>
2012-01-03 13:16:39 +0000

    real time clock fully implemented

--- trunk/src/rtc.c
+++ trunk/src/rtc.c
@@ -14,21 +14,33 @@
 #define RTC_REG_DH			0x04
 
 static void set_registers();
+static void byte_swap(Byte *ptr, size_t size);
 
 Byte regs[5];
 Byte regs_latched[5];
 
 int is_halted;
 
-//struct tm start_time;
-
 time_t start_time = 0;
+time_t halt_time = 0;
 
 void rtc_set_register(Byte r, Byte value) {
 	r -= RTC_SUBTRACT_REG;
-	regs[r] = value;
 	set_registers();
+	regs[r] = value;
+
+	// has the timer just been halted?
+	if ((!is_halted) && (regs[RTC_REG_DH] & 0x40)) {
+		halt_time = time(NULL);
+	} else
+	// has the timer just been unhalted?
+	if ((is_halted) && !(regs[RTC_REG_DH] & 0x40)) {
+		time_t time_halted = time(NULL) - halt_time;
+		start_time += time_halted;
+	}
+	
 	is_halted = (regs[RTC_REG_DH] & 0x40);
+
 	// if the timer is active, save the time
 	if (!is_halted) {
 		start_time = time(NULL);
@@ -39,17 +51,17 @@ void rtc_set_register(Byte r, Byte value) {
 		start_time -= (regs[RTC_REG_DH] & 0x01) * 60 * 60 * 24 * 256;
 		start_time -= ((regs[RTC_REG_DH] & 0x80) >> 7) * 60 * 60 * 24 * 256 * 2;
 	}
-	fprintf(stderr, "rtc_set_register: %hhx: %hhx\n", r, value);
+	//fprintf(stderr, "rtc_set_register: %hhx: %hhx\n", r, value);
 }
 
 Byte rtc_get_register(Byte r) {
 	r -= RTC_SUBTRACT_REG;
-	fprintf(stderr, "rtc_get_register: %hhx: %hhx\n", r, regs_latched[r]);
+	//fprintf(stderr, "rtc_get_register: %hhx: %hhx\n", r, regs_latched[r]);
 	return regs_latched[r];
 }
 
 void rtc_latch() {
-	fprintf(stderr, "rtc latch\n");
+	//fprintf(stderr, "rtc latch\n");
 	set_registers();
 	for (int i = 0; i < 0x05; i++) {
 		regs_latched[i] = regs[i];
@@ -58,6 +70,10 @@ void rtc_latch() {
 
 static void set_registers() {
 	time_t elapsed = time(NULL) - start_time;
+	if (is_halted) {
+		time_t time_halted = time(NULL) - halt_time;
+		elapsed -= time_halted;
+	}
 	if (elapsed < 0) {
 		fprintf(stderr, "real time clock error: clock was started in the future. Check system clock / time zone.\n");
 		elapsed = 0;
@@ -73,13 +89,28 @@ static void set_registers() {
 	regs[RTC_REG_DH] |= is_halted;
 }
 
-// FIXME: ENDIAN
+typedef struct {
+	uint64_t start_time;
+	uint32_t is_halted;
+	uint64_t halt_time;
+} __attribute__ ((packed)) Rtc_save_block;
+
 void rtc_save_sram(FILE *fp) {
-	uint64_t t = start_time;
+	Rtc_save_block save_block;
+	save_block.start_time = start_time;
+	save_block.is_halted = is_halted;
+	save_block.halt_time = halt_time;
 	size_t c;
-	c = fwrite(&t, sizeof(Byte), sizeof(t), fp);
-	if (c < sizeof(t)) {
-		fprintf(stderr, "error: wrote only %zu of %zu bytes of rtc to sram file\n", c, sizeof(t));
+
+	#ifdef WORDS_BIGENDIAN
+	byte_swap((Byte *)&save_block.start_time, sizeof(save_block.start_time));
+	byte_swap((Byte *)&save_block.is_halted, sizeof(save_block.is_halted));
+	byte_swap((Byte *)&save_block.halt_time, sizeof(save_block.halt_time));
+	#endif
+	
+	c = fwrite(&save_block, sizeof(Byte), sizeof(save_block), fp);
+	if (c < sizeof(save_block)) {
+		fprintf(stderr, "error: wrote only %zu of %zu bytes of rtc to sram file\n", c, sizeof(save_block));
 		perror("fwrite");
 		return;
 	}
@@ -87,14 +118,24 @@ void rtc_save_sram(FILE *fp) {
 
 void rtc_load_sram(FILE *fp) {
 	size_t c;
-	uint64_t t;
-	c = fread(&t, sizeof(Byte), sizeof(t), fp);
-	if (c != sizeof(t)) {
-		fprintf(stderr, "error: read only %zu of %zu bytes of rtc from sram file\n", c, sizeof(t));
+	Rtc_save_block save_block;
+	
+	c = fread(&save_block, sizeof(Byte), sizeof(save_block), fp);
+	if (c != sizeof(save_block)) {
+		fprintf(stderr, "error: read only %zu of %zu bytes of rtc from sram file\n", c, sizeof(save_block));
 		perror("fread");
 		return;
 	}
-	start_time = t;
+
+	#ifdef WORDS_BIGENDIAN
+	byte_swap((Byte *)&save_block.start_time, sizeof(save_block.start_time));
+	byte_swap((Byte *)&save_block.is_halted, sizeof(save_block.is_halted));
+	byte_swap((Byte *)&save_block.halt_time, sizeof(save_block.halt_time));
+	#endif
+
+	start_time = save_block.start_time;
+	is_halted = save_block.is_halted;
+	halt_time = save_block.halt_time;
 }
 
 void reset_rtc() {
@@ -104,4 +145,15 @@ void reset_rtc() {
 	}
 	is_halted = 0;
 	start_time = time(NULL);
+	halt_time = 0;
 }
+
+static void byte_swap(Byte *ptr, size_t size) {
+	Byte temp;
+	for (int i = 0; i < (size / 2); i++) {
+		temp = ptr[i];
+		ptr[i] = ptr[size - i - 1];
+		ptr[size - i - 1] = temp;
+	}
+}
+
